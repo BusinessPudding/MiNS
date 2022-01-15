8cd19e53399828dba7a01045abc02629471a090e
Yosuke Yamakawa <tech@y-yamakawa.com>
2022-01-15 18:04:23 +0900

    ノードのみ最低限の構成

--- src/App.js
+++ src/App.js
@@ -1,55 +1,21 @@
-import React, { useCallback, useState, useRef } from "react";
+import React from "react";
 import "./styles.css";
+
 import ReactFlow from "react-flow-renderer";
 
-const initialElements = [
+const elements = [
   {
     id: "1",
-    type: "input", // input node
-    data: { label: "Input Node" },
-    position: { x: 100, y: 0 },
+    type: "input",
+    data: { label: "Master Node" },
+    position: { x: 50, y: 50 },
   },
 ];
 
+const graphStyles = { width: "100%", height: "500px" };
+
+const BasicGraph = () => <ReactFlow elements={elements} style={graphStyles} />;
+
 export default function App() {
-  const [els, setEls] = useState(initialElements);
-  const yPos = useRef(0);
-
-  const addNode = useCallback(() => {
-    yPos.current += 50;
-    setEls((els) => {
-      console.log(els);
-      return [
-        ...els,
-        {
-          id: Math.random(),
-          position: { x: 100, y: yPos.current },
-          data: { label: "yo" },
-        },
-      ];
-    });
-  }, []);
-
-  const addEdge = useCallback(({ source, target }) => {
-    setEls((els) => {
-      console.log(source, target);
-      return [
-        ...els,
-        {
-          id: Math.random(),
-          source,
-          target,
-        },
-      ];
-    });
-  }, []);
-
-  return (
-    <div className="App">
-      <div style={{ height: 300 }}>
-        <ReactFlow elements={els} onConnect={addEdge} />
-      </div>
-      <button onClick={addNode}>Add</button>
-    </div>
-  );
+  return <BasicGraph />;
 }
